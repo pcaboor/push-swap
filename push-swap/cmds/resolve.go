da6092175022befe51d2e27fa56de45669e1d0a4
lmarchet <lucasmarchetti21@gmail.com>
2024-06-27 23:39:33 +0200

    seems to work properly

--- push-swap/cmds/resolve.go
+++ push-swap/cmds/resolve.go
@@ -18,6 +18,15 @@ func isSorted() bool {
 	return reflect.DeepEqual(Stacks.A, Ordered)
 }
 
+func isBSorted() bool {
+	sortedB := make([]int, len(Stacks.B))
+	copy(sortedB, Stacks.B)
+	sort.Slice(sortedB, func(i, j int) bool {
+		return sortedB[i] > sortedB[j]
+	})
+	return reflect.DeepEqual(Stacks.B, sortedB)
+}
+
 func areBothSorted() bool {
 	sortedA, sortedB := make([]int, len(Stacks.A)), make([]int, len(Stacks.B))
 	copy(sortedA, Stacks.A)
@@ -33,35 +42,105 @@ func Resolve() {
 	// Create a reference ordered to know the final state we need.
 	OrderReference()
 	c := 1
-	for !isSorted() && c < 10 {
+	for !isSorted() && c < 30 {
 		// Show evolution of ints in stacks.
 		DisplayResolve()
+		//Check if we can use Rra
+		if len(Stacks.A) > 2 {
+			doRra := true
+			for i := 0; i < len(Stacks.A); i++ {
+				if Stacks.A[len(Stacks.A)-1] > Stacks.A[i] {
+					doRra = false
+					break
+				}
+			}
+			if doRra {
+				Stacks.Rra()
+				fmt.Print(c)
+				fmt.Println(" - rra")
+				c++
+				continue
+			}
+		}
+		//Check if we can use Ra
+		if len(Stacks.A) > 2 {
+			doRa := true
+			for i := 0; i < len(Stacks.A); i++ {
+				if Stacks.A[0] < Stacks.A[i] {
+					doRa = false
+					break
+				}
+			}
+			if doRa {
+				Stacks.Ra()
+				fmt.Print(c)
+				fmt.Println(" - ra")
+				c++
+				continue
+			}
+		}
+		// Check if we can use Ss.
+		if len(Stacks.A) > 1 && len(Stacks.B) > 1 && Stacks.A[0] > Stacks.A[1] && Stacks.B[0] < Stacks.B[1] {
+			Stacks.Ss()
+			fmt.Print(c)
+			fmt.Println(" - ss")
+			c++
+			continue
+		}
 		// Check if we can use Sa.
 		if len(Stacks.A) > 1 && Stacks.A[0] > Stacks.A[1] {
 			Stacks.Sa()
-			fmt.Println(string(c+'0') + " - sa")
+			fmt.Print(c)
+			fmt.Println(" - sa")
+			c++
+			continue
+		}
+		// Check if we can use Sb.
+		if len(Stacks.B) > 1 && Stacks.B[0] < Stacks.B[1] {
+			Stacks.Sb()
+			fmt.Print(c)
+			fmt.Println(" - sb")
 			c++
 			continue
 		}
 		//Check if we can use Pb.
 		for i := 0; i+1 < len(Stacks.A); i++ {
 			if Stacks.A[i] > Stacks.A[i+1] {
-				for j := 0; j < i; j++ {
-					Stacks.Pb()
-					fmt.Println(string(c+'0') + " - pb")
-					c++
-				}
+				Stacks.Pb()
+				fmt.Print(c)
+				fmt.Println(" - pb")
+				c++
 				break
 			}
 		}
 		//Check if we can use Pa.
 		if len(Stacks.B) > 0 {
 			if areBothSorted() {
-				for i := 0; len(Stacks.B) > 0; i++ {
-					Stacks.Pa()
-					fmt.Println(string(c+'0') + " - pa")
-					c++
-				}
+				Stacks.Pa()
+				fmt.Print(c)
+				fmt.Println(" - pa")
+				c++
+				continue
+			}
+		}
+		//Check if we can use Rb
+		if len(Stacks.B) > 2 && !isBSorted() {
+			if Stacks.B[len(Stacks.B)-1] < Stacks.B[0] {
+				Stacks.Rb()
+				fmt.Print(c)
+				fmt.Println(" - rb")
+				c++
+				continue
+			}
+		}
+		//Check if we can use Rrb
+		if len(Stacks.B) > 2 {
+			if Stacks.B[len(Stacks.B)-1] > Stacks.B[0] {
+				Stacks.Rrb()
+				fmt.Print(c)
+				fmt.Println(" - rrb")
+				c++
+				continue
 			}
 		}
 	}
