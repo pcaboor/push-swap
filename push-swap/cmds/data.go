75919ba33d5c1e6c6792935387b5c1dc9b21ba96
lmarchet <lucasmarchetti21@gmail.com>
2024-06-27 20:25:47 +0200

    every cmds methods implemented

--- push-swap/cmds/data.go
+++ push-swap/cmds/data.go
@@ -18,4 +18,55 @@ func (s *StacksObject) Pb() {
 	}
 }
 
+func (s *StacksObject) Sa() {
+	if len(s.A) > 1 {
+		s.A[0], s.A[1] = s.A[1], s.A[0]
+	}
+}
+
+func (s *StacksObject) Sb() {
+	if len(s.B) > 1 {
+		s.B[0], s.B[1] = s.B[1], s.B[0]
+	}
+}
+
+func (s *StacksObject) Ss() {
+	s.Sa()
+	s.Sb()
+}
+
+func (s *StacksObject) Ra() {
+	if len(s.A) > 1 {
+		s.A = append(s.A[1:], s.A[0])
+	}
+}
+
+func (s *StacksObject) Rb() {
+	if len(s.B) > 1 {
+		s.B = append(s.B[1:], s.B[0])
+	}
+}
+
+func (s *StacksObject) Rr() {
+	s.Ra()
+	s.Rb()
+}
+
+func (s *StacksObject) Rra() {
+	if len(s.A) > 1 {
+		s.A = append([]int{s.A[len(s.A)-1]}, s.A[:len(s.A)-1]...)
+	}
+}
+
+func (s *StacksObject) Rrb() {
+	if len(s.B) > 1 {
+		s.B = append([]int{s.B[len(s.B)-1]}, s.B[:len(s.B)-1]...)
+	}
+}
+
+func (s *StacksObject) Rrr() {
+	s.Rra()
+	s.Rrb()
+}
+
 var Stacks StacksObject
